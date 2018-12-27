0d9a7934b6f41d775b5bb91e25d7b465ff77257b
Tamas K. Papp <tkpapp@gmail.com>
2018-12-27 20:51:18 +0100

    Move LaTeX substitutions to a separate file, clean up generating script.

---
+++ make-julia-latexsubs.jl
@@ -0,0 +1,34 @@
+#####
+##### Generate the file julia-latexsubs.el. Invoke from the shell as
+#####
+##### julia make-julia-latexsubs.jl
+#####
+
+@assert VERSION ≥ v"1"          # use a recent Julia version
+
+import REPL
+
+"""
+Write Emacs lisp code that populates the hash table named `varname` to `dest`, using
+key-value pairs from `src`.
+"""
+function write_latexsubs_hashtable(src, dest::IO, varname::AbstractString)
+    for (k, v) in sort!(collect(src), by = last)
+        ks = escape_string(k)
+        vs = escape_string(v)
+        if occursin(r"^\\U[0-9A-Fa-f]+$", vs)
+            # codepoints outside the BMP can be problematic in older Emacsen
+            cp = vs[3:end]
+            println(dest, "(let ((c (decode-char 'ucs #x$cp)))\n",
+                    "  (if c (puthash \"$ks\" (char-to-string c) $(varname))))")
+        else
+            println(dest, "(puthash \"$ks\" \"$vs\" $(varname))")
+        end
+    end
+end
+
+open("julia-latexsubs.el", "w") do io
+    write_latexsubs_hashtable(REPL.REPLCompletions.latex_symbols, io, "julia-latexsubs")
+end
+
+@info "generated latex substitutions"
